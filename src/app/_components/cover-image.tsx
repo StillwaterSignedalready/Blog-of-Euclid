be0c0e308ba8034683839b966c12fc8d1b08eb5e
canglang <doublecross@yeah.net>
2024-04-15 10:05:03 +0800

    feat: cover slide

--- src/app/_components/cover-image.tsx
+++ src/app/_components/cover-image.tsx
@@ -1,14 +1,18 @@
 import cn from "classnames";
+import type { RefObject } from 'react'
 import Link from "next/link";
 import Image from "next/image";
+import { CSSRuleObject } from "tailwindcss/types/config";
 
 type Props = {
   title: string;
   src: string;
   slug?: string;
+  style?: CSSRuleObject;
+  rootRef?: RefObject<HTMLImageElement>;
 };
 
-const CoverImage = ({ title, src, slug }: Props) => {
+const CoverImage = ({ title, src, slug, style, rootRef }: Props) => {
   const image = (
     <Image
       src={src}
@@ -20,8 +24,9 @@ const CoverImage = ({ title, src, slug }: Props) => {
       height={630}
     />
   );
+
   return (
-    <div className="sm:mx-0">
+    <div className="sm:mx-0" style={style} ref={rootRef}>
       {slug ? (
         <Link as={`/posts/${slug}`} href="/posts/[slug]" aria-label={title}>
           {image}
--- src/app/_components/hero-post.tsx
+++ src/app/_components/hero-post.tsx
@@ -1,15 +1,17 @@
-import Avatar from "@/app/_components/avatar";
+'use client'
 import CoverImage from "@/app/_components/cover-image";
-import { type Author } from "@/interfaces/author";
+import type { RefObject } from 'react'
 import Link from "next/link";
 import DateFormatter from "./date-formatter";
+import useImgCursorOffset from '@/lib/useImgCursorOffset'
+import { transform } from "lodash";
+
 
 type Props = {
   title: string;
   coverImage: string;
   date: string;
   excerpt: string;
-  author: Author;
   slug: string;
 };
 
@@ -18,13 +20,14 @@ export function HeroPost({
   coverImage,
   date,
   excerpt,
-  author,
   slug,
 }: Props) {
+  const [offset, imgContainerRef, imgRef] = useImgCursorOffset(3, -150);
+
   return (
     <section>
-      <div className="mb-8 md:mb-16 overflow-hidden" style={{ maxHeight: 400 }}>
-        <CoverImage title={title} src={coverImage} slug={slug} />
+      <div className="mb-8 md:mb-16 overflow-hidden" style={{ maxHeight: 400 }} ref={imgContainerRef}>
+        <CoverImage rootRef={imgRef as RefObject<HTMLImageElement>} title={title} src={coverImage} slug={slug} style={{ transform: `translate(0, ${offset}px)` }} />
       </div>
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
