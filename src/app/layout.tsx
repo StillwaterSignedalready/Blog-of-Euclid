06a5cd55e02641b6d6f61d2f7113f7dc2c1c5fa6
canglang <doublecross@yeah.net>
2024-05-09 16:38:54 +0800

    feat: header animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import Footer from "@/app/_components/footer";
-import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import SessionProvider from '@/app/SessionProvider'
@@ -9,11 +8,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
-  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
-  openGraph: {
-    images: [HOME_OG_IMAGE_URL],
-  },
+  title: `Blog of Wayne`,
+  description: `A fancy personal blog using Next.js.`,
 };
 
 export default async function RootLayout({
